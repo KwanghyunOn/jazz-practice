a8747026956a8bcce73d33ad46607b8754db9369
Kwanghyun On <kwanghyun.on@gmail.com>
2023-08-10 14:51:41 +0900

    Align center of the contents.

--- app/practices/layout.tsx
+++ app/practices/layout.tsx
@@ -1,3 +1,7 @@
 export default function PracticeLayout(props: { children: React.ReactNode }) {
-  return <div className="container mx-auto h-full">{props.children}</div>
+  return (
+    <div className="container mx-auto h-full flex justify-center">
+      <div>{props.children}</div>
+    </div>
+  )
 }
